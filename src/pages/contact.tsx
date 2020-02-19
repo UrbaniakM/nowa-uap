95592f29f338eb6e96d628c32b92cf5da2bec745
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2020-02-19 14:42:44 +0100

    Add more POC pages

---
+++ src/pages/contact.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const ContactPage: React.FC = () => {
+  return (
+    <div>
+      EVENTS - CURRENT AND ARCHIVAL
+    </div>
+  )
+}
+
+export default ContactPage;
---
+++ src/pages/events.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const EventsPage: React.FC = () => {
+  return (
+    <div>
+      EVENTS - CURRENT AND ARCHIVAL
+    </div>
+  )
+}
+
+export default EventsPage;
