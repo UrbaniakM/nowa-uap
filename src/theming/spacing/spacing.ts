63b46e1f1189b29e8b7d643cac6ac4cd6ab9ceb5
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2020-03-21 19:46:26 +0100

    Fix spacing function

--- src/theming/spacing/spacing.ts
+++ src/theming/spacing/spacing.ts
@@ -3,14 +3,14 @@ export type SpacingFunction = (baseMultiplier: number) => Spacing;
 
 export const spacingFunction: SpacingFunction =
   (baseMultiplier: number) =>
-    (val1: number, val2?: number, val3?: number, val4?: number) => {
-      if(!!val1 && !!val2 && !!val3 && val4) {
-        return `${baseMultiplier * val1}px ${baseMultiplier * val2}px ${baseMultiplier * val3}px ${baseMultiplier * val4}px`
-      } else if(!!val1 && !!val2 && !!val3) {
-        return `${baseMultiplier * val1}px ${baseMultiplier * val2}px ${baseMultiplier * val3}px`
+    (...args: any[]) => {
+      if(args.length === 4) {
+        return `${baseMultiplier * args[0]}px ${baseMultiplier * args[1]}px ${baseMultiplier * args[2]}px ${baseMultiplier * args[3]}px`
+      } else if(args.length === 3) {
+        return `${baseMultiplier * args[0]}px ${baseMultiplier * args[1]}px ${baseMultiplier * args[2]}px`
       }
-      else if(!!val1 && !!val2) {
-        return `${baseMultiplier * val1}px ${baseMultiplier * val2}px`
+      else if(args.length === 2) {
+        return `${baseMultiplier * args[0]}px ${baseMultiplier * args[1]}px`
       }
-      else return `${baseMultiplier * val1}px`
+      else return `${baseMultiplier * args[0]}px`
 }
