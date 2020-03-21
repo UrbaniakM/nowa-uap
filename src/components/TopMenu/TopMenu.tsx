c950e7b5deb7dad994131283fffd2eb29280b2ce
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2020-03-21 19:47:06 +0100

    Add base TopMenu Link styling

--- src/components/TopMenu/TopMenu.tsx
+++ src/components/TopMenu/TopMenu.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import styled from 'styled-components'
-import { Link } from 'gatsby';
+import { Link } from './components';
 
 const Wrapper = styled.div`
   display: flex;
--- src/components/TopMenu/components/Link/Link.tsx
+++ src/components/TopMenu/components/Link/Link.tsx
@@ -1,14 +1,21 @@
 import * as React from 'react';
-import { Link as RouterLink, LinkProps as RouterLinkProps } from '@reach/router';
+import { Link as RouterLink } from '@reach/router';
+import styled from 'styled-components';
 
 export interface LinkProps {
   to: string;
 }
 
+const StyledRouterLink = styled(RouterLink)`
+  color: ${props => props.theme.colorPalette.textSecondary};
+  text-decoration: none;
+  padding: ${props => props.theme.spacing(0, 2)};
+`;
+
 export const Link: React.FC<LinkProps> = ({ to, children }) => {
   return (
-    <RouterLink to={to}>
+    <StyledRouterLink to={to}>
       {children}
-    </RouterLink>
+    </StyledRouterLink>
   )
 }
