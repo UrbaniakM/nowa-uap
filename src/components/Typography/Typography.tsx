f2c6973330eb7bee3a24ce9ea37bf6febcc27e4c
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2020-03-22 12:09:48 +0100

    Add Typography styles based on variant prop

--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -1,12 +1,27 @@
 import * as React from 'react';
-import { Theme, TypographyStyle } from 'theming/';
+import { TypographyStyle } from 'theming/typography';
+import styled from 'styled-components';
 
-export interface TypographyProps { };
+export interface TypographyProps {
+  variant: TypographyStyle;
+};
 
-export const Typography: React.FC<TypographyProps> = () => {
+const StyledTypography = styled.div<TypographyProps>`
+  color: ${props => props.theme.typography[props.variant].color};
+  font-family: ${props => props.theme.typography[props.variant].fontFamily};
+  font-size: ${props => props.theme.typography[props.variant].fontSize};
+  font-style: ${props => props.theme.typography[props.variant].fontStyle};
+  font-weight: ${props => props.theme.typography[props.variant].fontWeight};
+`
+
+export const Typography: React.FC<TypographyProps> = ({
+  variant,
+  children
+}) => {
 
   return (
-    <div>
-    </div>
+    <StyledTypography variant={variant}>
+      {children}
+    </StyledTypography>
   )
 }
---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export * from './Footer';
+export * from './TopMenu';
+export * from './Typography';
