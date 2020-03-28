2b41b90173a9459aa41e6cca831c7196181e9146
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2020-03-28 03:20:02 +0100

    Modify Footer colors

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,11 +5,12 @@ import { Link } from '@reach/router';
 
 
 const FooterWrapper = styled.div`
-  background-color: grey;
+  background-color: ${props => props.theme.colorPalette.textPrimary};
   margin-top: ${props => props.theme.spacing(6)};
 `
 
 const FooterContent = styled.div`
+  color: ${props => props.theme.colorPalette.whitePrimary};
   display: flex;
   flex-direction: column;
   margin: ${props => props.theme.spacing(4, 6)};
