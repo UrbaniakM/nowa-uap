1efc6bbc732173d26ea13e98f10fa97367030e27
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2020-03-28 04:24:00 +0100

    Create Sections on Statute page

--- src/pages/statute.tsx
+++ src/pages/statute.tsx
@@ -10,6 +10,10 @@ const StatutePageWrapper = styled.div`
   }
 `
 
+const Section = styled.div`
+  margin-bottom: ${props => props.theme.spacing(4)};
+`
+
 const StatutePage: React.FC = () => {
   const { title: titleGoals, content: contentGoals } = useGoalsQuery();
   const { title: titleMethods, content: contentMethods } = useMethodsQuery();
@@ -17,20 +21,24 @@ const StatutePage: React.FC = () => {
   return (
     <StatutePageWrapper>
       <PageTitle>Cele i zasady działania Fundacji:</PageTitle>
-      <Typography variant='header3' textAlign='center'>§ 6</Typography>
-      <Typography variant='header3' textAlign='center'>{titleGoals}</Typography>
-      {contentGoals.map((point, index) => (
-        <Typography variant='body' key={index}>
-          {`${index+1}. ${point.item}`}
-        </Typography>
-      ))}
-      <Typography variant='header3' textAlign='center'>§ 7</Typography>
-      <Typography variant='header3' textAlign='center'>{titleMethods}</Typography>
-      {contentMethods.map((point, index) => (
-        <Typography variant='body' key={index}>
-          {`${index+1}. ${point.item}`}
-        </Typography>
-      ))}
+      <Section>
+        <Typography variant='header3' textAlign='center'>§ 6</Typography>
+        <Typography variant='header3' textAlign='center'>{titleGoals}</Typography>
+        {contentGoals.map((point, index) => (
+          <Typography variant='body' key={index}>
+            {`${index + 1}. ${point.item}`}
+          </Typography>
+        ))}
+      </Section>
+      <Section>
+        <Typography variant='header3' textAlign='center'>§ 7</Typography>
+        <Typography variant='header3' textAlign='center'>{titleMethods}</Typography>
+        {contentMethods.map((point, index) => (
+          <Typography variant='body' key={index}>
+            {`${index + 1}. ${point.item}`}
+          </Typography>
+        ))}
+      </Section>
     </StatutePageWrapper>
   )
 }
