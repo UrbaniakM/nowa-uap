b5981f646ca9203481e941e70ec8dcdfc7e0ed00
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2020-03-28 02:03:18 +0100

    Add textAlign prop to Typography component, fix font-size css

--- src/components/Typography/Typography.tsx
+++ src/components/Typography/Typography.tsx
@@ -1,26 +1,29 @@
 import * as React from 'react';
 import { TypographyStyle } from 'theming/typography';
-import styled from 'styled-components';
+import styled, { CSSProperties } from 'styled-components';
 
 export interface TypographyProps {
   variant: TypographyStyle;
+  textAlign?: CSSProperties['textAlign'];
 };
 
 const StyledTypography = styled.div<TypographyProps>`
   color: ${props => props.theme.typography[props.variant].color};
   font-family: ${props => props.theme.typography[props.variant].fontFamily};
-  font-size: ${props => props.theme.typography[props.variant].fontSize};
+  font-size: ${props => props.theme.typography[props.variant].fontSize}px;
   font-style: ${props => props.theme.typography[props.variant].fontStyle};
   font-weight: ${props => props.theme.typography[props.variant].fontWeight};
+  text-align: ${props => props.textAlign}
 `
 
 export const Typography: React.FC<TypographyProps> = ({
   variant,
+  textAlign,
   children
 }) => {
 
   return (
-    <StyledTypography variant={variant}>
+    <StyledTypography variant={variant} textAlign={textAlign}>
       {children}
     </StyledTypography>
   )
