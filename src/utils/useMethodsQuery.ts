7207cbaf62ca02153f4609743f9f6607b12eb20e
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2020-03-28 02:27:58 +0100

    Create useMethodsQuery hook

---
+++ src/utils/useMethodsQuery.ts
@@ -0,0 +1,28 @@
+import { graphql, useStaticQuery } from "gatsby";
+
+export const methodsQuery = graphql`
+  query MethodsQuery {
+    methodsYaml {
+      title
+      content {
+        item
+      }
+    }
+  }
+`
+
+export interface MethodsQuery {
+  methodsYaml: {
+    title: string;
+    content: {
+      item: string;
+    }[]
+  }
+}
+
+export const useMethodsQuery = () => {
+  const { methodsYaml } = useStaticQuery<MethodsQuery>(methodsQuery);
+
+
+  return methodsYaml;
+}
