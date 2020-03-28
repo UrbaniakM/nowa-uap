854f19d675f7a07d9124cf98e00b562301ef6d5f
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2020-03-28 04:50:45 +0100

    Add hours and tel to contact  page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -6,14 +6,29 @@ const Section = styled.div`
   margin: ${props => props.theme.spacing(1, 0)};
 `
 
+const Row = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ContactPageWrapper = styled.div`
+  width: 70%;
+`
+
 const ContactPage: React.FC = () => {
   return (
-    <div>
+    <ContactPageWrapper>
       <PageTitle>Kontakt</PageTitle>
       <Typography variant='header3'>Fundacja Nowa UAP</Typography>
       <Section>
-        <Typography variant='body' color='textSecondary'>ul. Nazwa Ulicy XY</Typography>
-        <Typography variant='body' color='textSecondary'>01-234 Poznań</Typography>
+        <Row>
+          <Typography variant='body' color='textSecondary'>ul. Nazwa Ulicy XY</Typography>
+          <Typography variant='body' color='textSecondary'>tel.: +48 12 345 67 89</Typography>
+        </Row>
+        <Row>
+          <Typography variant='body' color='textSecondary'>01-234 Poznań</Typography>
+          <Typography variant='body' color='textSecondary'>(pon. – pt. 8:00 – 16:00)</Typography>
+        </Row>
       </Section>
       <Section>
       <Typography variant='body'>NIP: 0000000000</Typography>
@@ -24,7 +39,7 @@ const ContactPage: React.FC = () => {
       <Typography variant='body' color='textSecondary'>Fundacja nie prowadzi działalności gospodarczej i jest organizacją pożytku publicznego.</Typography>
       <Typography variant='body'>Nr konta: 00 0000 0000 0000 0000 0000 0000</Typography>
       </Section>
-    </div>
+    </ContactPageWrapper>
   )
 }
 
