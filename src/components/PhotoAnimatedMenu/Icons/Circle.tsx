ca591060ab5848be91a5b42079146b731a3b7c53
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2020-04-21 15:51:39 +0200

    Add Circle svg

---
+++ src/components/PhotoAnimatedMenu/Icons/Circle.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+export const Circle: React.FC = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 80 80">
+      <g id="Symbols" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" strokeOpacity="0.87">
+        <g id="News/Section-prompt/Icon/Black" stroke="currentColor" strokeWidth="4">
+          <circle id="Oval" cx="40" cy="40" r="38" />
+        </g>
+      </g>
+    </svg>
+  )
+}
+
+
