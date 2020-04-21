02350b9e34d0e94d491e61081f9a1dd62d3522bd
Mateusz Urbaniak <31575249+UrbaniakM@users.noreply.github.com>
2020-04-21 16:02:32 +0200

    Create PhotoUAP component

---
+++ src/components/PhotoAnimatedMenu/PhotoUAP.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  position: relative;
+  margin-bottom: ${props => props.theme.spacing(4)};
+`
+
+const Photo = styled.img`
+  object-fit: cover;
+  width: 100%;
+  max-height: 768px;
+  filter: contrast(0.5);
+`
+
+export const PhotoUAP: React.FC = ({ children }) => {
+  return (
+    <Container>
+      <Photo src='/uap_photo/UAP_Photo_1.png' />
+      {children}
+    </Container>
+  )
+}
